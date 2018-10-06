acf60509faa008e987713779934f6dc46b34e362
Kim Minyeob <stankim0525@gmail.com>
2018-10-06 21:53:53 +0900

    40%

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -6,6 +6,7 @@ import {ROWS, COLS} from './config';
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
   this.joints = [
+    {x: 3, y: 0},
     {x: 2, y: 0},
     {x: 1, y: 0},
     {x: 0, y: 0},
@@ -17,29 +18,69 @@ function SnakeGameLogic() {
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
-
-  
+  this.auto = 'up'
+  // this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y - 1 })
+  // this.joints.pop()
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
+  this.auto = 'down'
+  // this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y + 1 })
+  // this.joints.pop()
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
+  this.auto = 'left'
+  // this.joints.unshift({x: this.joints[0].x -1, y: this.joints[0].y })
+  // this.joints.pop()
   console.log('left');
+
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
+  this.auto = 'right'
+  // this.joints.unshift({x: this.joints[0].x +1, y: this.joints[0].y })
+  // this.joints.pop()
   console.log('right');
 }
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
+   if(this.auto === 'right'){
+    this.joints.unshift({x: this.joints[0].x +1, y: this.joints[0].y })
+    // this.joints.pop()
+   }
+   else if(this.auto === 'left'){
+    this.joints.unshift({x: this.joints[0].x -1, y: this.joints[0].y })
+    // this.joints.pop()
+   }
+   else if( this.auto === 'down'){
+    this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y + 1 })
+    // this.joints.pop()
+   }
+   else{
+    this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y - 1 })
+    // this.joints.pop()
+   }
+  
+
+  // if먹이를 만났을때는 꼬리 안떨어짐 else 꼬리 떨어짐
+// if(this.fruit.x, this.fruit,y === this.joints[0].x, this.joints[0].y){
+//   this.fruit={x:10, y:10}
+// }
+if(this.fruit.x === this.joints[0].x && this.fruit.y === this.joints[0].y ){
+  this.fruit={x:10, y:10}
+}
+else{
+  this.joints.pop()
+}
   // 게임이 아직 끝나지 않았으면 `true`를 반환
+  +
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
   return true;
