0e9ca708e71e881abda186bf3ad8cf50dcf6bc12
Kim Minyeob <stankim0525@gmail.com>
2018-10-08 16:32:15 +0900

    almost Please enter the commit message for your changes. Lines starting

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -12,6 +12,8 @@ function SnakeGameLogic() {
     {x: 0, y: 0},
   ];
 
+  //마지막으로 누른 상태를 기억해서 자동으로 가는 상태
+  this.auto = 'right'
   // 먹이의 좌표
   this.fruit = {x: 3, y: 5};
 }
@@ -50,39 +52,104 @@ SnakeGameLogic.prototype.right = function() {
 }
 
 SnakeGameLogic.prototype.nextState = function() {
+  console.log(`nextState`);
+
+  let newFruit = {}
+  let newHead
+  
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
-   if(this.auto === 'right'){
-    this.joints.unshift({x: this.joints[0].x +1, y: this.joints[0].y })
-    // this.joints.pop()
-   }
-   else if(this.auto === 'left'){
-    this.joints.unshift({x: this.joints[0].x -1, y: this.joints[0].y })
-    // this.joints.pop()
-   }
-   else if( this.auto === 'down'){
-    this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y + 1 })
-    // this.joints.pop()
-   }
-   else{
-    this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y - 1 })
-    // this.joints.pop()
-   }
+  
+  if(this.auto === 'up'){
+    newHead = {
+      x: this.joints[0].x,
+      y: this.joints[0].y - 1
+    }
+  }
+  else if(this.auto === 'down'){
+    newHead = { 
+      x: this.joints[0].x,
+      y: this.joints[0].y + 1
+    }
+  }
+  else if(this.auto === 'right'){
+    newHead = {
+      x: this.joints[0].x + 1,
+      y: this.joints[0].y
+    }
+  }
+  else {
+    newHead = {
+      x: this.joints[0].x - 1,
+      y: this.joints[0].y
+    }
+  }
+
+  
+  if( newHead.y >= ROWS ||newHead.y < 0 ||  
+    newHead.x >= COLS || newHead.x < 0 ||  
+    this.joints.some
+    (joint => joint.x === newHead.x && 
+     joint.y === newHead.y )){
+  return false
+}
+
+if (newHead.x === this.fruit.x && newHead.y === this.fruit.y) {
+  do {
+    newFruit.x = Math.floor(Math.random() * COLS);
+    newFruit.y = Math.floor(Math.random() * ROWS);
+    this.fruit = newFruit;
+  } while (
+    (newFruit.x === newHead.x && newFruit.y === newHead.y) ||
+    this.joints.some(
+      joint => joint.x === newFruit.x && joint.y === newFruit.y
+    )
+  );
+} else {
+  this.joints.pop();
+}
+
+
+  //  *if(this.auto === 'right'){
+  //   this.joints.unshift({x: this.joints[0].x +1, y: this.joints[0].y })
+  //   // this.joints.pop()
+  //  }
+  //  else if(this.auto === 'left'){
+  //   this.joints.unshift({x: this.joints[0].x -1, y: this.joints[0].y })
+  //   // this.joints.pop()
+  //  }
+  //  else if( this.auto === 'down'){
+  //   this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y + 1 })
+  //   // this.joints.pop()
+  //  }
+  //  else{
+  //   this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y - 1 })
+  //   // this.joints.pop()
+  //  }*
   
 
   // if먹이를 만났을때는 꼬리 안떨어짐 else 꼬리 떨어짐
 // if(this.fruit.x, this.fruit,y === this.joints[0].x, this.joints[0].y){
 //   this.fruit={x:10, y:10}
 // }
-if(this.fruit.x === this.joints[0].x && this.fruit.y === this.joints[0].y ){
-  this.fruit={x:10, y:10}
-}
-else{
-  this.joints.pop()
-}
+// if(this.fruit.x === this.joints[0].x && 
+//    this.fruit.y === this.joints[0].y ){
+//  newFruit.x = Math.floor(Math.random() * (28-1)+1)
+//  newFruit.y = Math.floor (Math.random() * (18-1)+1)
+// this.fruit = newFruit
+
+// }
+// else{
+//   this.joints.pop()
+// }
+// this.fruit = newFruit
+
+
   // 게임이 아직 끝나지 않았으면 `true`를 반환
-  +
+  
   // 게임이 끝났으면 `false`를 반환
-  console.log(`nextState`);
+
+  //x,y 좌표가 col,row 보다 클때 죽음
+  this.joints.unshift(newHead);
   return true;
 }
 
